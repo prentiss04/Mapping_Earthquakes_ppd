7116afc6d7df4adbdbe4567adc632ce0ff600836
GoBearsCatsLions <prentiss.iv@gmail.com>
2020-01-13 11:49:16 -0800

    lines

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -64,21 +64,35 @@ let cityData = cities;
 // Add console.log to check to see if our code is working.
 console.log("working");
 // We create the map object with options.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 // We create the tile layer that will be the background of our map.
 // Add a marker to the map for Los Angeles, CA
 // // let cities = L.marker([34.0522, -118.2437]).addTo(map);
 // L.circle([34.0522, -118.2437], {radius: 100}).addTo(map);
 // L.circleMarker([34.0522, -118.2437], {radius: 300, color: "black", fillColor:'#ffffa1'}).addTo(map);
 
-cityData.forEach(function(city) {
-	console.log(city)
-L.circleMarker(city.location,{radius: city.population/100000})
-.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <h2> <h3>Population " + city.population.toLocaleString() + "</h3>")
-.addTo(map);
-});
+// Coordinates for each point to be used in the line
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+];
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+
+// Create a polyline using the line coordinates and make the line red
+L.polyline(line, {
+    color: "yellow"
+}).addTo(map);
+
+// cityData.forEach(function(city) {
+// 	console.log(city)
+// L.circleMarker(city.location,{radius: city.population/100000})
+// .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <h2> <h3>Population " + city.population.toLocaleString() + "</h3>")
+// .addTo(map);
+// });
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
