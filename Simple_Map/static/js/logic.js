577af1aa14092f2b5a195a5762e35f3a30903b8f
GoBearsCatsLions <prentiss.iv@gmail.com>
2020-01-13 10:35:49 -0800

    circle markers

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -13,14 +13,70 @@
 // // Then we add our 'graymap' tile layer to the map.
 // streets.addTo(map);
 
+// // An array containing each city's location, state, and population.
+// let cities = [{
+//     location: [40.7128, -74.0059],
+//     city: "New York City",
+//     state: "NY",
+//     population: 8398748
+//   },
+//   {
+//     location: [41.8781, -87.6298],
+//     city: "Chicago",
+//     state: "IL",
+//     population: 2705994
+//   },
+//   {
+//     location: [29.7604, -95.3698],
+//     city: "Houston",
+//     state: "TX",
+//     population: 2325502
+//   },
+//   {
+//     location: [34.0522, -118.2437],
+//     city: "Los Angeles",
+//     state: "CA",
+//     population: 3990456
+//   },
+//   {
+//     location: [33.4484, -112.0740],
+//     city: "Phoenix",
+//     state: "AZ",
+//     population: 1660272
+//   }
+//   ];
+
+// Get data from cities.js
+let cityData = cities;
+
+
+
+// // Loop through the cities array and create one marker for each city.
+// cities.forEach(function(city) {
+//     console.log(city)
+// });
+
+// cities.forEach(function(city) {
+// 	console.log(city)
+// L.marker(city.location).addTo(map);
+// });
+
 // Add console.log to check to see if our code is working.
 console.log("working");
 // We create the map object with options.
 let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 // We create the tile layer that will be the background of our map.
 // Add a marker to the map for Los Angeles, CA
+// // let cities = L.marker([34.0522, -118.2437]).addTo(map);
 // L.circle([34.0522, -118.2437], {radius: 100}).addTo(map);
-L.circleMarker([34.0522, -118.2437], {radius: 300, color: "black", fillColor:'#ffffa1'}).addTo(map);
+// L.circleMarker([34.0522, -118.2437], {radius: 300, color: "black", fillColor:'#ffffa1'}).addTo(map);
+
+cityData.forEach(function(city) {
+	console.log(city)
+L.circleMarker(city.location,{radius: city.population/100000})
+.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <h2> <h3>Population " + city.population.toLocaleString() + "</h3>")
+.addTo(map);
+});
 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -29,7 +85,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
 });
 // Then we add our 'streets' tile layer to the map.
 streets.addTo(map);
-darkmap.addTo(map);
+// darkmap.addTo(map);
 
 
 // // Add console.log to check to see if our code is working.
