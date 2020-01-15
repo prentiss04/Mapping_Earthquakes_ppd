8e03643a1c500bf9ac3ca8bf173f07c006fc1c2e
GoBearsCatsLions <prentiss.iv@gmail.com>
2020-01-15 11:30:30 -0800

    polygons

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -134,14 +134,14 @@ let line = [
 // .addTo(map);
 // });
 
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
 });
 
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
@@ -149,15 +149,15 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Light: light,
-    Dark: dark
+    "Streets": streets,
+    "Satellite Streets": satelliteStreets
   };
 
 
   let map = L.map('mapid', {
-    center: [44.0, -80.0],
-    zoom: 2,
-    layers: [light]
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [satelliteStreets]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map
@@ -173,8 +173,11 @@ let airportData = "https://raw.githubusercontent.com/prentiss04/Mapping_Earthqua
 //   L.geoJson(data).addTo(map);
 // });
 
-// Accessing the Toronto airline routes GeoJSON URL
-let torontoData = "https://raw.githubusercontent.com/prentiss04/Mapping_Earthquakes_ppd/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+// // Accessing the Toronto airline routes GeoJSON URL
+// let torontoData = "https://raw.githubusercontent.com/prentiss04/Mapping_Earthquakes_ppd/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/prentiss04/Mapping_Earthquakes_ppd/Mapping_GeoJSON_Linestrings/torontoNeighborhoods.json";
 
 // Create a style for the lines.
 let myStyle = {
@@ -182,18 +185,27 @@ let myStyle = {
     weight: 2
 }
 
-// Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+// // Grabbing our GeoJSON data.
+// d3.json(torontoData).then(function(data) {
+//     console.log(data);
+//   // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJson(data, {
+//       style: myStyle,
+//       onEachFeature: function(feature, layer){
+//           layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
+//     }
+// })
+// .addTo(map);
+// });
+
+// Grabbing our GeoJSON data for polygons
+d3.json(torontoHoods).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data, {
-      style: myStyle,
-      onEachFeature: function(feature, layer){
-          layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
-    }
-})
-.addTo(map);
+  L.geoJson(data).addTo(map);
 });
+
+
 
 
 // L.geoJSON(airportData, {
